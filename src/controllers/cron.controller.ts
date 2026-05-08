d0058b5ee05b18df215d158210b019cd72cedf03
agustinmusumeci <agu.musumeci@gmail.com>
2026-05-07 22:39:15 -0300

    fix: Activate scraper at cron job. Update internships mock

--- src/controllers/cron.controller.ts
+++ src/controllers/cron.controller.ts
@@ -11,41 +11,7 @@ export class CronController {
       const uploader = new UploadController();
       const userService = new UserService();
 
-      const internships = [
-        {
-          id: 619,
-          arm: "41/26",
-          city: "CORDOBA CAPITAL",
-          rrhh: "Juan Asnal",
-          interview_timetable: "A convenir.",
-          knowledge:
-            "El rol está orientado al apoyo en el monitoreo y la gestión de los Pisos Tecnológicos (CAIID), espacios comunitarios de acceso digital ubicados en distintos barrios populares de la ciudad.",
-          requirements: "Requiere saber SQL",
-          payment: 450000,
-          timetable: "20hs semanales, de lunes a viernes, horario a convenir.",
-          position:
-            "Asistente técnico-comunitario en Pisos Tecnológicos (CAIID-Centros de Acceso a Internet para la Integración Digital) – Proyecto Ranchada IP-Internet popular/ Área de Integración Digital.",
-          benefits:
-            "La pasantía ofrece la posibilidad de integrarse a un proyecto de inclusión digital con trabajo en territorio, adquirir experiencia en gestión de proyectos sociales y fortalecer capacidades en el cruce entre tecnología y comunidad, en articulación con organizaciones sociales e instituciones públicas.",
-          interns: 1,
-          workplace: "En sede central, ubicada en Julio A. Roca 584, barrio Güemes",
-          modality: "PRESENCIAL",
-          link: "Sin link",
-          mail: "mutualcarlosmugica@gmail.com",
-          observations: "+ viáticos",
-          company_id: "ASOCIACION MUTUAL CARLOS MUGICA",
-          university_id: null,
-          created_at: new Date("2026-04-14T01:17:14.591Z"),
-          company: {
-            id: "ASOCIACION MUTUAL CARLOS MUGICA",
-            name: "ASOCIACION MUTUAL CARLOS MUGICA",
-          },
-          careers: ["SISTEMAS"],
-          timeSinceCreated: { time: "Hace 4 d", color: "#00a27d" },
-        },
-      ];
-
-      // const { internships } = await uploader.uploadData();
+      const { internships } = await uploader.uploadData();
 
       // We dont have any internship to notify to
       if (internships.length === 0) {
@@ -73,7 +39,7 @@ export class CronController {
         const userId = data.userId;
         const userInternships = data.internships.map(Number);
 
-        // await userService.createNotification(userId, userInternships);
+        await userService.createNotification(userId, userInternships);
       }
     } catch (e) {
       console.error(e);
